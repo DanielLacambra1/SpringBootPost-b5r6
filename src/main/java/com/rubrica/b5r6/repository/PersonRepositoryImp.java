caa4dd017974fb8d57ca7f0f36fd4c4a76f25a04
DanielLacambra1 <lacambra.sidan21@zaragoza.salesianos.edu>
2023-02-17 23:10:45 +0100

    funcion persist modificada

--- src/main/java/com/rubrica/b5r6/repository/PersonRepositoryImp.java
+++ src/main/java/com/rubrica/b5r6/repository/PersonRepositoryImp.java
@@ -9,7 +9,9 @@ public class PersonRepositoryImp implements PersonRepository {
 
     @Override
     public void persist(PersonDTO person) {
-    
+        System.out.println("Persisting person: " + person);
+        System.out.println(person.getNombre() + " " + person.getApellido() + " " 
+        + person.getFnac() + " " + person.getProfesion() + " " + person.getSalario() );
     }
     
 }
